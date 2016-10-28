1b72273812dec668578e672c4f010993842ea597
Sune Andersen <sunadtudk@gmail.com>
2016-10-28 16:32:27 +0200

    Update build_irods.sh

--- build_irods.sh
+++ build_irods.sh
@@ -1,12 +1,10 @@
+#/tmp/irods-4.1.9/packaging/build.sh clean
+/tmp/irods-4.1.9/packaging/build.sh icat postgres > /dev/null
+/tmp/irods-4.1.9/packaging/build.sh -r -v resource > /dev/null
+/tmp/irods-4.1.9/packaging/build.sh icommands > /dev/null
 
-/tmp/irods-4.1.9/packaging/build.sh clean
-/tmp/irods-4.1.9/packaging/build.sh icat postgres
-/tmp/irods-4.1.9/packaging/build.sh -r -v resource
-/tmp/irods-4.1.9/packaging/build.sh icommands
-
-#cd .. ; cd ~/irods-4.1.9/build/
-#git clone https://github.com/irods/irods_auth_plugin_kerberos.git
-#cd irods_auth_plugin_kerberos/packaging/
-#./build.sh ; cd .. ; cd ..
-#rpm -ivh irods_auth_plugin_kerberos/build/irods-auth-plugin-krb-1.3-*
-
+cd /tmp/irods-4.1.9/
+git clone https://github.com/irods/irods_auth_plugin_kerberos.git
+/tmp/irods-4.1.9/irods_auth_plugin_kerberos/packaging/build.sh
+#rpm -ivh /tmp/irods-4.1.9/irods_auth_plugin_kerberos/build/irods-auth-plugin-krb-1.3-*
+ 
